f095ce59d689b1c33ee70ea58379332ff814470f
mercyjae <famakinwamercy20@gmail.com>
2022-11-04 03:40:27 -0700

    add webview

--- lib/resume_app.dart
+++ lib/resume_app.dart
@@ -1,4 +1,7 @@
+
 import 'package:flutter/material.dart';
+import 'package:hng_task2/web_view_page.dart';
+
 
 class ResumeApp extends StatefulWidget {
   const ResumeApp({Key? key}) : super(key: key);
@@ -6,8 +9,20 @@ class ResumeApp extends StatefulWidget {
   @override
   State<ResumeApp> createState() => _ResumeAppState();
 }
-
+ 
 class _ResumeAppState extends State<ResumeApp> {
+  int selectedIndex = 0;
+  void _handleURLButtonPress(
+    BuildContext context,
+    String url,
+  ) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => WebViewPage(
+                  url,
+                )));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +38,7 @@ class _ResumeAppState extends State<ResumeApp> {
               Container(
                 height: 200,
                 width: double.infinity,
-                color: Color(0xFFFA624F),
+                color: const Color(0xFFFA624F),
               ),
               Positioned(
                 top: 120,
@@ -49,7 +64,7 @@ class _ResumeAppState extends State<ResumeApp> {
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(50),
-                        image: DecorationImage(
+                        image: const DecorationImage(
                             image: AssetImage("assets/Tatiana.jpg"),
                             fit: BoxFit.cover),
                       ))),
@@ -59,33 +74,33 @@ class _ResumeAppState extends State<ResumeApp> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
+                    const Text(
                       "Anuoluwapo Famakinwa",
                       style:
                           TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 5,
                     ),
-                    Text(
+                    const Text(
                       "Mobile developer",
                       style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 15,
                           color: Colors.grey),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 15,
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         buildCounters(context, '1.6K', 'Likes'),
-                        SizedBox(
+                        const SizedBox(
                           width: 12,
                         ),
                         buildCounters(context, '3.5K', 'Following'),
-                        SizedBox(
+                        const SizedBox(
                           width: 12,
                         ),
                         buildCounters(context, '15K', 'Followers'),
@@ -98,42 +113,50 @@ class _ResumeAppState extends State<ResumeApp> {
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('LOCATION',
-                  style:
-                  TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                Row(
-                  children: [Icon(Icons.location_on), Text("Lagos, Nigeria",
-
-                    style:
-                    TextStyle(fontWeight: FontWeight.w400, fontSize: 15),)],
+                const Text(
+                  'LOCATION',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
                 ),
-                SizedBox(height: 10),
-                Text('ON THE WEB',
-
-                    style:
-                    TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
                 Row(
                   children: [
-                    BuildLogo('assets/twitter.png'),
-                    BuildLogo('assets/linkedin.png'),
-                    BuildLogo('assets/github.jpg')
+                    const Icon(Icons.location_on),
+                    const Text(
+                      "Lagos, Nigeria",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w400, fontSize: 15),
+                    )
                   ],
                 ),
-                SizedBox(height: 10),
-                Text("ABOUT",
-
-                  style:
-                  TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
-                Text("I'm an active, dynamic mobile developer (flutter and dart), "
-                    "capable of creating excellent analytical skills with good problem-solving attitude'"
-                    " and who is able to  perform in a team environment of talented IT professional in "
-                    "creating mobile apps to the highest standards.",
-
-                  style:
-                  TextStyle(fontWeight: FontWeight.w400, fontSize: 15),),
-                SizedBox(
+                const SizedBox(height: 10),
+                const Text(
+                  'ON THE WEB',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                Row(children: [
+      
+                  BuildLogo(
+                      'assets/twitter.png', 'https://twitter.com/_mercyjae'),
+                  BuildLogo('assets/linkedin.png',
+                      'https://www.linkedin.com/in/famakinwa-anuoluwapo'),
+                  BuildLogo('assets/github.jpg', 'https://github.com/mercyjae')
+                  //  )],
+                ]),
+                const SizedBox(height: 10),
+                const Text(
+                  "ABOUT",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                const Text(
+                  "I'm an active, dynamic mobile developer (flutter and dart), "
+                  "capable of creating excellent analytical skills with good problem-solving attitude'"
+                  " and who is able to  perform in a team environment of talented IT professional in "
+                  "creating mobile apps to the highest standards.",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15),
+                ),
+                const SizedBox(
                   height: 30,
                 ),
                 Row(
@@ -141,28 +164,27 @@ class _ResumeAppState extends State<ResumeApp> {
                   children: [
                     ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                          minimumSize: Size(100, 40),
-                          backgroundColor: Color(0xFFFA624F),
+                          minimumSize: const Size(100, 40),
+                          backgroundColor: selectedIndex == 0 ? const Color(0xFFFA624F) : Colors.white,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10)),
                         ),
                         child: const Text("Chat"),
-                        onPressed: () {}),
+                        onPressed: () {selectedIndex == 0;}),
                     ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                          minimumSize: Size(100, 40),
-                          backgroundColor: Color(0xFFFA624F),
+                          minimumSize: const Size(100, 40),
+                          backgroundColor:selectedIndex == 1? Colors.white: const Color(0xFFFA624F),
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10)),
                         ),
                         child: const Text("Message"),
-                        onPressed: () {}),
+                        onPressed: () {selectedIndex == 1;}),
                   ],
                 ),
               ],
             ),
           ),
-
         ],
       ),
     );
@@ -177,34 +199,39 @@ class _ResumeAppState extends State<ResumeApp> {
       children: [
         Text(
           firstLine,
-          style: TextStyle(
+          style: const TextStyle(
               fontSize: 15, fontWeight: FontWeight.bold, color: Colors.grey),
         ),
         const SizedBox(height: 8),
         Text(
           secondLine,
-          style: TextStyle(
+          style: const TextStyle(
               fontSize: 15, fontWeight: FontWeight.bold, color: Colors.grey),
         ),
-        SizedBox(
+        const SizedBox(
           width: 10,
         )
       ],
     );
   }
 
-    Widget BuildLogo (String image){
-    return Container(
-        height: 50,
-        width: 50,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(50),
-          image: DecorationImage(
-              image: AssetImage(image),
-
-
-              fit: BoxFit.cover),
-        ));
+  Widget BuildLogo(String image, String url) {
+    return InkWell(
+      onTap: () {
+        _handleURLButtonPress(
+          context,
+          url,
+        );
+      },
+      child: Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(50),
+            image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+          )),
+    );
   }
 }
+
